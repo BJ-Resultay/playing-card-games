6ce4ed86d6ad1ea484bb5e7fd6ff9cb4fec3df4b
BJ Resultay <bjkent.resultay@gmail.com>
2023-08-16 22:40:49 -0400

    Refactor tests for card

--- tests/unit/src/general_tests/card_test.py
+++ tests/unit/src/general_tests/card_test.py
@@ -3,27 +3,40 @@
 # Revision History:
 #	resultay | 15-08-23 | Initial version
 
+import pytest
 from src.constants import Face
 from src.constants import Suit
 from src.general import Card
 
-def test_frozen():
+@pytest.fixture()
+def card():
+    """fixture returns ace of spades"""
+    return Card(Face.ACE, Suit.SPADE)
+
+def test_frozen(card):
     """trying to override face value raises an error"""
-    card = Card(Face.ACE, Suit.SPADE)
+    # card = Card(Face.ACE, Suit.SPADE)
     try:
         # cheater switching out the card
         card.face = Face.KING
     except AttributeError:
         assert True
 
-def test_flip():
+def test_not_frozen(card):
+    """overriding point value does not raise error"""
+    # card = Card(Face.ACE, Suit.SPADE)
+    assert card.points == 0
+    card.points = 1
+    assert card.points == 1
+
+def test_flip(card):
     """card flips over"""
-    card = Card(Face.ACE, Suit.SPADE)
+    # card = Card(Face.ACE, Suit.SPADE)
     assert card.face_down
     card.flip()
     assert not card.face_down
 
-def test_face_value():
+def test_face_value(card):
     """returns face value"""
-    card = Card(Face.ACE, Suit.SPADE)
+    # card = Card(Face.ACE, Suit.SPADE)
     assert card.face_value() == 'A\u2660'
