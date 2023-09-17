3714037cfdb622be0630c5276b3d39ba434894ad
BJ Resultay <bjkent.resultay@gmail.com>
2023-09-17 16:06:20 -0400

    Refactor blackjack player test

--- tests/unit/src/games/blackjack_tests/blackjack_player_test.py
+++ tests/unit/src/games/blackjack_tests/blackjack_player_test.py
@@ -15,26 +15,126 @@ def player() -> BlackjackPlayer:
     """fixture returns blackjack player"""
     return BlackjackPlayer('Bill Malone')
 
-def should_double_down_soft(points: int, mask: list[int]):
-    """test_double_down helper"""
-    card = Card(Face.ACE, None, points)
-    should_double_down(card, mask)
+class TestDoubleDown():
+    """test double down related functions"""
+    def should_double_down(
+        self,
+        card: Card,
+        mask: list[int],
+    ) -> None:
+        """test_should_double_down helper"""
+        player = BlackjackPlayer('new')
+        player.hit(card)
+        for i in range(2, 12):
+            if i in mask:
+                assert player.should_double_down(i)
+            else:
+                assert not player.should_double_down(i)
 
-def should_double_down_hard(points: int, mask: list[int]):
-    """test_double_down helper"""
-    card = Card(None, None, points)
-    should_double_down(card, mask)
+    @pytest.mark.parametrize(
+        'points, mask',
+        [
+            (13, [5, 6]),
+            (14, [5, 6]),
+            (15, range(4, 7)),
+            (16, range(4, 7)),
+            (17, range(3, 7)),
+            (18, range(2, 7)),
+            (19, [6]),
+            (20, []),
+            (21, []),
+        ]
+    )
+    def test_should_double_down_soft(
+        self,
+        ace: Card,
+        mask: list[int],
+        points: int,
+    ):
+        """player should double down with soft hand"""
+        # Soft 19 doubles against dealer 6
+        # Soft 18 doubles against dealer 2 through 6
+        # Soft 17 doubles against dealer 3 through 6
+        # Soft 16 doubles against dealer 4 through 6
+        # Soft 15 doubles against dealer 4 through 6
+        # Soft 14 doubles against dealer 5 through 6
+        # Soft 13 doubles against dealer 5 through 6
+        ace.points = points
+        self.should_double_down(ace, mask)
 
-def should_double_down(card: Card, mask: list[int]):
-    """test_double_down helper"""
-    player = BlackjackPlayer('new')
-    card.flip()
-    player.hit(card)
-    for i in range(2, 12):
-        if i in mask:
-            assert player.should_double_down(i)
-        else:
-            assert not player.should_double_down(i)
+    should_not_double_down_hard_param = [
+        pytest.param(i, [])
+        for i in range(9, 22)
+        if i not in range(2, 12)
+    ]
+
+    @pytest.mark.parametrize(
+        'points, mask',
+        [
+            (10, range(2, 10)),
+            (11, range(2, 12)),
+            (9, range(3, 7)),
+        ] + should_not_double_down_hard_param
+    )
+    def test_should_double_down_hard(
+        self,
+        mask: list[int],
+        non_ace: Card,
+        points: int,
+    ):
+        """player should double down with hard hand"""
+        # Hard 11 always doubles.
+        # Hard 10 doubles against dealer 2 through 9
+        # Hard 9 doubles against dealer 3 through 6
+        non_ace.points = points
+        self.should_double_down(non_ace, mask)
+
+    def test_can_double_down_true(
+        self,
+        ace: Card,
+        player: BlackjackPlayer,
+    ):
+        """player can double down"""
+        player.hit(ace)
+        player.hit(ace)
+        assert player.can_double_down()
+
+    def test_can_double_down_false(
+        self,
+        ace: Card,
+        player: BlackjackPlayer,
+    ):
+        """player cannot double down"""
+        assert not player.can_double_down()
+        player.hit(ace)
+        player.hit(ace)
+        player.bet_chips(player.STARTING_CHIPS)
+        assert not player.can_double_down()
+
+    def test_double_down(
+        self,
+        ace: Card,
+        non_ace: Card,
+        player: BlackjackPlayer,
+    ):
+        """player doubled down"""
+        player.bet_chips(5)
+        player.hit(ace)
+        player.hit(ace)
+        player.double_down(non_ace)
+        assert non_ace in player.hand
+        assert player.hand.double
+        assert player.hand.end
+        assert player.chips == player.STARTING_CHIPS - 5 * 2
+
+    def test_double_down_error(
+        self,
+        ace: Card,
+        player: BlackjackPlayer
+    ):
+        """player cannot double down raises error"""
+        with pytest.raises(BlackjackError):
+            player.double_down(ace)
 
 def should_split(face: Face, mask: list[int]):
     """test_should_split helper"""
@@ -79,20 +179,6 @@ def test_blackjack_unnatural(
     player.hit(non_ace)
     player.hands.append('some hand')
     assert not player.blackjack()
-
-def test_can_double_down_true(ace: Card, player: BlackjackPlayer):
-    """player can double down"""
-    player.hit(ace)
-    player.hit(ace)
-    assert player.can_double_down()
-
-def test_can_double_down_false(ace: Card, player: BlackjackPlayer):
-    """player cannot double down"""
-    assert not player.can_double_down()
-    player.hit(ace)
-    player.hit(ace)
-    player.bet_chips(player.STARTING_CHIPS)
-    assert not player.can_double_down()
 
 def test_can_hit_true(non_ace: Card, player: BlackjackPlayer):
     """player can hit"""
@@ -178,26 +264,6 @@ def test_discard_cards(ace: Card, player: BlackjackPlayer):
     assert len(player.hand) == 0
     assert len(player.hands) == 1
 
-def test_double_down(
-    ace: Card,
-    non_ace: Card,
-    player: BlackjackPlayer,
-):
-    """player doubled down"""
-    player.bet_chips(5)
-    player.hit(ace)
-    player.hit(ace)
-    player.double_down(non_ace)
-    assert non_ace in player.hand
-    assert player.hand.double
-    assert player.hand.end
-    assert player.chips == player.STARTING_CHIPS - 5 * 2
-
-def test_double_down_error(ace: Card, player: BlackjackPlayer):
-    """player cannot double down raises error"""
-    with pytest.raises(BlackjackError):
-        player.double_down(ace)
-
 def test_hit(non_ace: Card, player: BlackjackPlayer):
     """player hits"""
     player.hit(non_ace)
@@ -223,45 +289,6 @@ def test_increase_invalid_stat(player: BlackjackPlayer):
     """invalid stat raises error"""
     with pytest.raises(AttributeError):
         player.increase_stat(1)
-
-def test_should_double_down_soft():
-    """player should double down with soft hand"""
-    # Soft 19 doubles against dealer 6
-    # Soft 18 doubles against dealer 2 through 6
-    # Soft 17 doubles against dealer 3 through 6
-    # Soft 16 doubles against dealer 4 through 6
-    # Soft 15 doubles against dealer 4 through 6
-    # Soft 14 doubles against dealer 5 through 6
-    # Soft 13 doubles against dealer 5 through 6
-    should_double_down_soft(13, [5, 6])
-    should_double_down_soft(14, [5, 6])
-    should_double_down_soft(15, range(4, 7))
-    should_double_down_soft(16, range(4, 7))
-    should_double_down_soft(17, range(3, 7))
-    should_double_down_soft(18, range(2, 7))
-    should_double_down_soft(19, [6])
-
-def test_should_not_double_down_soft():
-    """player should not double down with soft hand"""
-    for i in [20, 21]:
-        should_double_down_soft(i, [])
-
-
-def test_should_double_down_hard():
-    """player should double down with hard hand"""
-    # Hard 11 always doubles.
-    # Hard 10 doubles against dealer 2 through 9
-    # Hard 9 doubles against dealer 3 through 6
-    should_double_down_hard(10, range(2, 10))
-    should_double_down_hard(11, range(2, 12))
-    should_double_down_hard(9, range(3, 7))
-
-def test_should_not_double_down_hard():
-    """player should not double down with hard hand"""
-    for i in range(4, 22):
-        if i in range(9, 12):
-            continue
-        should_double_down_hard(i, [])
 
 def test_should_split():
     """player should split"""
