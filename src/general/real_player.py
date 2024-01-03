594d41eb63f9ad29a9763fcb6dbbe018f9dc4546
BJ Resultay <bjkent.resultay@gmail.com>
2024-01-03 00:10:44 -0500

    Add real player bets

--- src/general/real_player.py
+++ src/general/real_player.py
@@ -4,8 +4,13 @@
 #	resultay | 27-12-23 | Initial version
 
 from logging import getLogger
+from math import inf
 from Levenshtein import distance
-from src.general.player import Player
+from src.general.player import (
+    InsufficientAmount,
+    NotPositive,
+    Player,
+)
 
 LOGGER = getLogger(__name__)
 
@@ -16,24 +21,33 @@ class RealPlayer(Player):
         super().__init__(name)
         self.logger = LOGGER
 
-    def multiple_choice(self, prompt: str, choices: list[str]) -> str:
+    def __input(self) -> str:
         """function takes player input
+
+        Returns:
+            str: raw_input
+        """
+        raw_input = None
+        while not raw_input:
+            raw_input = input('> ')
+        self.logger.info('Raw input %s', raw_input)
+        return raw_input
+
+    def user_choice(self, prompt: str, choices: list[str]) -> str:
+        """function interprets input into closest choice
 
         Args:
             prompt (str): question for user
             choices (list[str]): valid choices
 
         Returns:
-            str: closest choice
+            str: closest choice of valid choices
         """
         print(prompt)
         choices.sort()
         for choice in choices:
             print(f'* {choice}')
-        raw_input = ''
-        while not raw_input:
-            raw_input = input('> ')
-        self.logger.info('Raw input %s', raw_input)
+        raw_input = self.__input()
 
         # if choice starts with input
         filtered_choices = [choice for choice in choices if choice.startswith(raw_input)]
@@ -46,3 +60,57 @@ class RealPlayer(Player):
         print(levenshtein_distances)
         minimum_position = levenshtein_distances.index(min(levenshtein_distances))
         return choices[minimum_position]
+
+    def user_chips(self) -> None:
+        """function takes player bet
+        """
+        prompt = f"""
+        Current Balance {self.chips}
+        Place bet.
+        """
+        chips = inf
+        clean = None
+        while clean is None:
+            try:
+                chips = self.user_float(prompt)
+                self.bet_chips(chips)
+                clean = False
+            except (InsufficientAmount, NotPositive) as e:
+                self.logger.warning(e)
+                print(e)
+
+    def user_float(self, prompt: str) -> float:
+        """function interprets input as float
+
+        Args:
+            prompt (str): question for user
+
+        Returns:
+            float: input as float
+        """
+        print(prompt)
+        number = None
+        while number is None:
+            try:
+                number = float(self.__input())
+            except ValueError:
+                print(prompt)
+        return number
+
+    def user_integer(self, prompt: str) -> int:
+        """function interprets input as int
+
+        Args:
+            prompt (str): question for user
+
+        Returns:
+            int: input as integer
+        """
+        print(prompt)
+        number = None
+        while number is None:
+            try:
+                number = int(self.__input())
+            except ValueError:
+                print(prompt)
+        return number
--- tests/unit/src/general_tests/real_player_test.py
+++ tests/unit/src/general_tests/real_player_test.py
@@ -4,8 +4,11 @@
 #	resultay | 27-12-23 | Initial version
 
 from pytest import MonkeyPatch
+from pytest_mock import MockerFixture
 import pytest
 from src.general.real_player import RealPlayer
+
+NUMBER = '777'
 
 @pytest.fixture()
 def actions() -> list[str]:
@@ -24,43 +27,81 @@ def real_player(monkeypatch: MonkeyPatch) -> RealPlayer:
     monkeypatch.undo() # breakpoints use bulitin input
     return player
 
-def test_get_input(
+def test_get_choice(
     actions: list[str],
     monkeypatch: MonkeyPatch,
     real_player: RealPlayer
 ):
-    """get input from player"""
+    """get choice from player"""
     monkeypatch.setattr('builtins.input', lambda _ : actions[0])
-    action = real_player.multiple_choice('', actions)
+    action = real_player.user_choice('', actions)
     assert action == actions[0]
 
-def test_get_no_input(
+def test_get_no_choice(
     actions: list[str],
     monkeypatch: MonkeyPatch,
     real_player: RealPlayer
 ):
-    """get no from player"""
+    """get no input from player"""
     user_input = iter(['', actions[1]])
     monkeypatch.setattr('builtins.input', lambda _ : next(user_input))
-    action = real_player.multiple_choice('', actions)
+    action = real_player.user_choice('', actions)
     assert action == actions[1]
 
-def test_get_shortened_input(
+def test_get_shortened_choice(
     actions: list[str],
     monkeypatch: MonkeyPatch,
     real_player: RealPlayer
 ):
-    """get shortened input from player"""
+    """get shortened choice from player"""
     monkeypatch.setattr('builtins.input', lambda _ : 'action')
-    action = real_player.multiple_choice('', actions)
+    action = real_player.user_choice('', actions)
     assert action == actions[0]
 
-def test_get_shorthand_input(
+def test_get_shorthand_choice(
     actions: list[str],
     monkeypatch: MonkeyPatch,
     real_player: RealPlayer
 ):
-    """get shorthand input from player"""
+    """get shorthand choice from player"""
     monkeypatch.setattr('builtins.input', lambda _ : 'a2')
-    action = real_player.multiple_choice('', actions)
+    action = real_player.user_choice('', actions)
     assert action == actions[1]
+
+def test_get_bet(mocker: MockerFixture, real_player: RealPlayer):
+    """get bet from player"""
+    mocker.patch.object(real_player, 'user_float', return_value = float(NUMBER))
+    real_player.user_chips()
+    assert real_player.chips == real_player.STARTING_CHIPS - float(NUMBER)
+
+def test_get_invalid_bet(mocker: MockerFixture, real_player: RealPlayer):
+    """get invalid bet from player"""
+    mocker.patch.object(real_player, 'user_float', side_effect = [-420, float(NUMBER)])
+    real_player.user_chips()
+    assert real_player.chips == real_player.STARTING_CHIPS - float(NUMBER)
+
+def test_get_float(monkeypatch: MonkeyPatch, real_player: RealPlayer):
+    """get float from player"""
+    monkeypatch.setattr('builtins.input', lambda _ : NUMBER)
+    number = real_player.user_float('')
+    assert number == float(NUMBER)
+
+def test_get_invalid_float(monkeypatch: MonkeyPatch, real_player: RealPlayer):
+    """get invalid float from player"""
+    user_input = iter(['', NUMBER])
+    monkeypatch.setattr('builtins.input', lambda _ : next(user_input))
+    number = real_player.user_float('')
+    assert number == float(NUMBER)
+
+def test_get_integer(monkeypatch: MonkeyPatch, real_player: RealPlayer):
+    """get integer from player"""
+    monkeypatch.setattr('builtins.input', lambda _ : NUMBER)
+    number = real_player.user_integer('')
+    assert number == int(NUMBER)
+
+def test_get_invalid_integer(monkeypatch: MonkeyPatch, real_player: RealPlayer):
+    """get invalid integer from player"""
+    user_input = iter(['', NUMBER])
+    monkeypatch.setattr('builtins.input', lambda _ : next(user_input))
+    number = real_player.user_integer('')
+    assert number == int(NUMBER)
